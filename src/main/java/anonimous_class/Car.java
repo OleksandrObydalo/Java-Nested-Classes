fe80f512c6140a16bb5fbc3fd63b13c477411c8b
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-11 21:48:40 +0300

    Add anonimous class example

---
+++ src/main/java/anonimous_class/Car.java
@@ -0,0 +1,44 @@
+package anonimous_class;
+
+public class Car {
+    private String model;
+    private int year;
+    private Engine engine;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+        // Anonymous inner class for setting up the engine
+        this.engine = new Engine() {
+            @Override
+            public void start() {
+                System.out.println("The engine of the car " + model + " has started.");
+            }
+
+            @Override
+            public void stop() {
+                System.out.println("The engine of the car " + model + " has stopped.");
+            }
+        };
+    }
+
+    public void startCar() {
+        engine.start();
+    }
+
+    public void stopCar() {
+        engine.stop();
+    }
+
+    private abstract class Engine {
+        public abstract void start();
+        public abstract void stop();
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car("ToyotaCamry", 2022);
+        car.startCar();
+        car.stopCar();
+    }
+}
+
