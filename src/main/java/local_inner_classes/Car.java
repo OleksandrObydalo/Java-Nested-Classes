39a884319f38053d08ebdfeabcc1ef4d1c89de9b
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-12 16:54:01 +0300

    Add Compressor class to package local_inner_classes

--- src/main/java/local_inner_classes/Car.java
+++ src/main/java/local_inner_classes/Car.java
@@ -5,28 +5,42 @@ public class Car {
     private String make;
     private String model;
 
-    public void start(int horsepower) {
+    public void start(int horsepower, Compressor compressor) {
         System.out.println("Starting the car");
 
         class Engine {
             private int horsepower;
+            private Compressor compressor;
 
-            public Engine(int horsepower) {
+            public Engine(int horsepower, Compressor compressor) {
                 this.horsepower = horsepower;
+                this.compressor = compressor;
             }
 
             public void rev() {
                 System.out.println("The engine is revving at " + horsepower + " horsepower.");
+                System.out.println("Compressor has diameter " + compressor.diameter);
             }
         }
 
-        Engine engine = new Engine(horsepower);
+        Engine engine = new Engine(horsepower, compressor);
+        compressor.diameter = 200;
         engine.rev();
+    }
+
+    class Compressor{
+        public int diameter;
+
+        public Compressor(int diameter) {
+            this.diameter = diameter;
+        }
     }
 
     public static void main(String[] args) {
         Car car = new Car();
-        car.start(1000);
+        car.start(1000, car.new Compressor(100));
     }
 }
+
+
 
